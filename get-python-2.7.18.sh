3d9af13afb562ee4f9248007795975d61f15935a
Daniel Maixner <xskipy@gmail.com>
2021-04-20 11:59:56 +0200

    typo

--- get-python-2.7.18.sh
+++ get-python-2.7.18.sh
@@ -1,12 +1,11 @@
 # usage
 # bash get-python-2.7.18.sh
 sudo apt-get remove --purge python
-sudo apt-get remove --purge python2
 sudo apt-get update
 sudo apt-get install build-essential checkinstall -y
 sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev -y
 cd /usr/src
-sudo wget hthttps://www.python.org/ftp/python/2.7.18/Python-2.7.18.tgz
+sudo wget https://www.python.org/ftp/python/2.7.18/Python-2.7.18.tgz
 sudo tar xzf Python-2.7.18.tgz
 cd Python-2.7.18
 sudo ./configure --enable-optimizations

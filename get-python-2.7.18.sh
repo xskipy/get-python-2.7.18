4e79f5ed6d44b13b958ba2a4f40eac4aa24a3f8f
Daniel Maixner <xskipy@gmail.com>
2021-04-20 11:18:36 +0200

    initial commit

---
+++ get-python-2.7.18.sh
@@ -0,0 +1,14 @@
+# usage
+# bash get-python-2.7.18.sh
+sudo apt-get update
+sudo apt-get install build-essential checkinstall -y
+sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev -y
+cd /usr/src
+sudo wget hthttps://www.python.org/ftp/python/2.7.18/Python-2.7.18.tgz
+sudo tar xzf Python-2.7.18.tgz
+cd Python-2.7.18
+sudo ./configure --enable-optimizations
+sudo make test
+sudo make install
+sudo rm -rf /usr/src/Python-2.7.18*
+
